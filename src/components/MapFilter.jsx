9aff883cb37afafba4fabdf254a345edc039c329
rpanos <rpanos76@yahoo.com>
2023-06-17 21:30:24 -0700

    Filters

---
+++ src/components/MapFilter.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Form from 'react-bootstrap/Form';
+
+import '../App.css'; // TEMP!
+
+
+
+function MapFilter({minSize, handleMinSize, minTot, handleMinTot}) {
+
+    return (
+    <div className="filterContainter">
+          {/* <label>Limit: </label>
+          <input type="text" value={limit}></input> */}
+          <div className='filterField'>
+            <label>Minimum Neighborhood Size: </label>
+            <Form.Select aria-label="Minimum Neighborhood Size" value={minSize} onChange={handleMinSize}>
+              <option value="0">Open this select menu</option>
+              <option value="400000">Small</option>
+              <option value="800000">Medium</option>
+              <option value="1200000">Large</option>
+              <option value="1600000">Mega Large</option>
+            </Form.Select>
+          </div>
+          <div className='filterField'>
+            <label>Minimum Total Accidents: </label>
+            <Form.Select aria-label="Minimum Total Accidents" value={minTot} onChange={handleMinTot}>
+              <option value="0">Open this select menu</option>
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="15">15</option>
+              <option value="20">20</option>
+              <option value="25">25</option>
+            </Form.Select>
+          </div>
+        </div>
+    )
+}
+
+export default MapFilter;
--- src/components/NeighborhoodApp.jsx
+++ src/components/NeighborhoodApp.jsx
@@ -3,6 +3,7 @@ import { useJsApiLoader } from '@react-google-maps/api';
 
 import getNeighborhoodData from '../api/neighborhoodAPI'
 import LeafletMap from './LeafletMap'
+import MapFilter from './MapFilter'
 import '../App.css'; // TEMP!
 
 const NeighborhoodApp = () => {
@@ -14,14 +15,24 @@ const NeighborhoodApp = () => {
     const [neighborhoods, setNeighborhoods] = React.useState([])
     const [featuredNeighborhoods, setFeaturedNeighborhoodProperties] = React.useState({})
 
+    const [minTot, setMinTot] = React.useState(null)
+    const [minSize, setMinSize] = React.useState(null)
+
     React.useEffect(() => {
 
-        const newData = getNeighborhoodData({limit: 200, minSize: 200000, minTot: 5}) //, sortBy: "total"
+        const newData = getNeighborhoodData({limit: 200, minSize: minSize, minTot: minTot}) //, sortBy: "total"
         if (newData && newData.length) {
           console.log("newData is now ", newData.length);
         }
         setNeighborhoods(newData);
-      }, []); 
+      }, [minSize, minTot]); 
+
+      const handleMinTot=(e) => {
+        setMinTot(e.target.value)
+      }
+      const handleMinSize=(e) => {
+        setMinSize(e.target.value)
+      }
 
       const handleNeighborhoodClick = (e, id, name, chartData) => {
         if (!featuredNeighborhoods[id]) {
@@ -37,13 +48,18 @@ const NeighborhoodApp = () => {
 
       return isLoaded ? (
         <div className="appContainer">
-        
+            <MapFilter 
+                minTot={minTot}
+                handleMinTot={handleMinTot}
+                minSize={minSize}
+                handleMinSize={handleMinSize}
+            />
           <div className="mapContainter">
             <LeafletMap
-              neighborhoodsData={neighborhoods}
-              handleNeighborhoodClick={handleNeighborhoodClick}
-              featuredNeighborhoods={featuredNeighborhoods}
-              >
+                neighborhoodsData={neighborhoods}
+                handleNeighborhoodClick={handleNeighborhoodClick}
+                featuredNeighborhoods={featuredNeighborhoods}
+                >
             </LeafletMap>
           </div>
         </div>
