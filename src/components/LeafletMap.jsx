f45ec72dc681340d7b6d89d273ffe0912e804dd1
rpanos <rpanos76@yahoo.com>
2023-06-17 21:11:48 -0700

    Map with polygons highlighting

---
+++ src/components/LeafletMap.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { MapContainer, TileLayer, Polygon } from 'react-leaflet'
+import '../App.css'; // TEMP!
+
+const initialCenter=[
+    38.908511,
+    -77.005802
+  ];
+
+const LeafletMap = ({neighborhoodsData, featuredNeighborhoods, handleNeighborhoodClick}) => {
+    const zoomLevel = 12;
+
+    return (
+    <MapContainer 
+        center={initialCenter} 
+        zoom={zoomLevel} 
+        scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      />
+
+        { neighborhoodsData.length > 0 && neighborhoodsData.map(( nieghborhoodData ) => {
+          const polyOptions =  { color: 'purple' };
+          if (featuredNeighborhoods[nieghborhoodData["id"]]) {
+            polyOptions.color = 'orange'; // IMPROVE?
+          }
+
+          return(<Polygon
+              positions={nieghborhoodData['geometry']['coordinates'][0]}
+              pathOptions={polyOptions}
+              eventHandlers={{
+                click: (e) => handleNeighborhoodClick(e, nieghborhoodData["id"], nieghborhoodData["properties"]["name"], nieghborhoodData["chart_data"])
+              }}
+              key={nieghborhoodData["id"]}
+            />)
+        })}   
+    </MapContainer>
+    )
+}
+
+export default LeafletMap;
--- src/components/NeighborhoodApp.jsx
+++ src/components/NeighborhoodApp.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import getNeighborhoodData from '../api/neighborhoodAPI'
+import { useJsApiLoader } from '@react-google-maps/api';
 
+import getNeighborhoodData from '../api/neighborhoodAPI'
+import LeafletMap from './LeafletMap'
+import '../App.css'; // TEMP!
 
 const NeighborhoodApp = () => {
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyDxf4S4yOTXh7I9pHFX0af8x__83_rjovc"
+    })
 
     const [neighborhoods, setNeighborhoods] = React.useState([])
+    const [featuredNeighborhoods, setFeaturedNeighborhoodProperties] = React.useState({})
 
     React.useEffect(() => {
 
@@ -15,6 +23,31 @@ const NeighborhoodApp = () => {
         setNeighborhoods(newData);
       }, []); 
 
+      const handleNeighborhoodClick = (e, id, name, chartData) => {
+        if (!featuredNeighborhoods[id]) {
+          const newFeaturedNeighborhoodProperties = {...featuredNeighborhoods}; // confirm!  Do deep copy?
+          newFeaturedNeighborhoodProperties[id] = { chartData, name };
+          setFeaturedNeighborhoodProperties(newFeaturedNeighborhoodProperties)
+        } else {
+          const newFeaturedNeighborhoodProperties = {...featuredNeighborhoods};
+          delete newFeaturedNeighborhoodProperties[id];
+          setFeaturedNeighborhoodProperties(newFeaturedNeighborhoodProperties)
+        }
+      }
+
+      return isLoaded ? (
+        <div className="appContainer">
+        
+          <div className="mapContainter">
+            <LeafletMap
+              neighborhoodsData={neighborhoods}
+              handleNeighborhoodClick={handleNeighborhoodClick}
+              featuredNeighborhoods={featuredNeighborhoods}
+              >
+            </LeafletMap>
+          </div>
+        </div>
+    ) : <></>
 
 }
 
